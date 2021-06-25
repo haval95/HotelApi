d032241e70771a474cfd108d2d84360700c035b5
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-06-25 14:33:26 +0300

    initiating project

---
+++ HotelApi/HotelApi/Controllers/rootController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelApi.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class rootController: ControllerBase
+    {
+        [HttpGet(Name = nameof(Get))]
+       public IActionResult Get()
+        {
+            var respone = new
+            {
+
+                href = Url.Link(nameof(Get), null)
+            };
+            return (Ok(respone));
+        }
+    }
+}
